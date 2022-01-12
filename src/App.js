f48bbda28393cb18e3f61c0fb4c6b3ce6a9e1660
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-01-12 20:53:24 +0500

    added Carousel component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Carousel />
     </div>
   );
 }
