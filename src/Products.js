1b4924836a0f56c85884b088b06691ea76f92465
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-01-12 22:39:22 +0500

    added products array and passed it to products's state

---
+++ src/Products.js
@@ -0,0 +1,28 @@
+import img from "./img/IMG_9356.jpg";
+
+export const productsArray = [
+  {
+    id: 1,
+    title: "Lofer # 1",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ecommerce",
+    img: img,
+  },
+  {
+    id: 2,
+    title: "Lofer # 2",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ecommerce",
+    img: img,
+  },
+  {
+    id: 3,
+    title: "Lofer # 3",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ecommerce",
+    img: img,
+  },
+  {
+    id: 4,
+    title: "Lofer # 4",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ecommerce",
+    img: img,
+  },
+];
--- src/components/ProductsListing.jsx
+++ src/components/ProductsListing.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { CardGroup } from "react-bootstrap";
-import img from "../img/IMG_9356.jpg";
 import SingleProductCard from "./SingleProductCard";
-const text =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit ecommerce";
+import { productsArray } from "../Products";
 
 const ProductsListing = () => {
+  const [products, setProducts] = useState(productsArray);
+
   return (
     <div>
       <CardGroup className="card-group">
-        <SingleProductCard img={img} title="Lofer # 1" text={text} />
-        <SingleProductCard img={img} title="Lofer # 2" text={text} />
-        <SingleProductCard img={img} title="Lofer # 3" text={text} />
-        <SingleProductCard img={img} title="Lofer # 4" text={text} />
+        {products.map((product) => (
+          <SingleProductCard
+            key={product.id}
+            img={product.img}
+            title={product.title}
+            text={product.text}
+          />
+        ))}
       </CardGroup>
     </div>
   );
