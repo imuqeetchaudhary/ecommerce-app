24eb034ab9ad424db8573f405e822728f839dd32
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-01-12 19:04:00 +0500

    added Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+const NavbarComp = () => {
+  return (
+    <>
+      <Navbar fixed="top" bg="dark" variant="dark">
+        <Container fluid="md">
+          <Navbar.Brand href="#home">A to Z Dressing 👕🩳🧣</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#products">Products</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavbarComp;
