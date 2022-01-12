7bddd68265fed1f500c101e2420026b3c5ceecd7
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-01-12 22:26:48 +0500

    added footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import CarouselCard from "./components/CarouselCard";
 import ProductsListing from "./components/ProductsListing";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <CarouselCard />
       <ProductsListing />
+      <Footer />
     </div>
   );
 }
